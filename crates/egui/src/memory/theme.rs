affcb1053e6bf487d3348631e88efae6b24f36f8
Tau Gärtli <git@tau.garden>
2024-07-17 12:05:33 +0200

    Move `Theme` to egui

---
+++ crates/egui/src/memory/theme.rs
@@ -0,0 +1,23 @@
+/// Dark or Light theme.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+pub enum Theme {
+    /// Dark mode: light text on a dark background.
+    Dark,
+
+    /// Light mode: dark text on a light background.
+    Light,
+}
+
+impl Theme {
+    /// Get the egui visuals corresponding to this theme.
+    ///
+    /// Use with [`egui::Context::set_visuals`].
+    #[deprecated]
+    pub fn egui_visuals(self) -> crate::Visuals {
+        match self {
+            Self::Dark => crate::Visuals::dark(),
+            Self::Light => crate::Visuals::light(),
+        }
+    }
+}
